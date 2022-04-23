67e6ab950118fdbc1fe842d64e51026c03afc306
Arpan Chattopadhyay <arpanchattopadhyay23@gmail.com>
2022-04-23 14:51:40 +0530

    complete all options

--- neovim/.config/nvim/lua/options/init.lua
+++ neovim/.config/nvim/lua/options/init.lua
@@ -1,11 +1,13 @@
 local options = {
-	scrolloff = 8,
-	encoding = "utf-8",
-	cursorline = true,
-	number = true,
-	relativenumber = true,
-	autoread = true,
-	termguicolors = true,
+	scrolloff = 8, -- keeps 8 space width at the bottom
+  completeopt = { "menuone", "noselect" }, -- for cmp
+  clipboard = "unnamedplus", -- whatever is yanked or deleted is put in the '+' registry
+  hlsearch = true, -- highlights searched words
+	encoding = "utf-8", -- basic file encoding
+	cursorline = true, -- highlights the line the cursor is on
+	relativenumber = true, -- enables relative line numbers
+	autoread = true, -- Update neovim if a file changes from outside of neovim
+	termguicolors = true,  -- Enables 24-bit RGB color in the Terminal UI
 	wrap = false,
 	linebreak = true,
 	breakindent = true,
@@ -17,8 +19,6 @@ local options = {
  	swapfile = false,
  	writebackup = false,
  	undofile = true,
- 	splitright = true,
- 	ignorecase = true,
  	smartcase = true,
  	confirm = true,
  	signcolumn = 'yes',
@@ -30,8 +30,13 @@ local options = {
   smartindent = true,
   smarttab = true,
   foldlevel = 99,
-  updatetime = 300
+  updatetime = 300,
+  splitright = true,
+  splitbelow = true,
+  ignorecase = true,
 }
+
+vim.opt.iskeyword:append("-")
 
 for option_name, option_value in pairs(options) do
 	vim.opt[option_name] = option_value
